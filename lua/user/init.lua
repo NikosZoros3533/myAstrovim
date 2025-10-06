f2ba45fb297bbe4787f093cea0996364eaa48dea
NikosZoros3533 <nikos7kozani@gmail.com>
2025-10-06 14:22:17 +0300

    my little config

---
+++ lua/user/init.lua
@@ -0,0 +1,9 @@
+
+-- %LOCALAPPDATA%\nvim\lua\user\init.lua
+
+vim.opt.termguicolors = true
+vim.opt.clipboard = "unnamedplus"
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.signcolumn = "yes"
+vim.opt.grepprg = "rg --vimgrep --smart-case"
